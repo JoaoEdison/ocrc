70939b31242ac693c63eb72800be935fc8e8a59c
Joao <upload@pegasus.inf.br>
2023-05-20 22:54:28 -0300

    Cleaning up

--- src/view.c
+++ src/view.c
@@ -22,17 +22,11 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <dirent.h>
-#include <errno.h>
 
 cmp_strings(x, y)
 void *x, *y;
 {
 	return strcmp(*(char**)x, *(char**)y);
-}
-without_points_dirs(f)
-struct dirent *f;
-{
-	return strcmp(f->d_name, ".") && strcmp(f->d_name, "..");
 }
 
 struct {
@@ -54,17 +48,17 @@ main(argc, argv)
 char *argv[];
 {
 	void rec_read_file();
-	struct stat st;
 	char **files, *c;
+	struct stat st;
 	int i, end;
 
 	if (argc < 2) {
 		puts("Usage: view -a -d -h -o -v -R -H image");
 		return 1;
 	}
-	files = malloc(sizeof(char*) * (argc-1));
 	end = 0;
 	flags.view = 1;
+	files = malloc(sizeof(char*) * (argc-1));
 	while (--argc)
 		if ((*++argv)[0] == '-')
 			for (c=argv[0]+1; *c; c++)
@@ -97,8 +91,8 @@ char *argv[];
 			files[end++] = *argv;
 	qsort(files, end, sizeof(char*), cmp_strings);
 	if (!getcwd(cwd, sizeof(cwd))) {
-		perror("[getcwd]");
-		return 8;
+		perror("[main] getcwd");
+		return 3;
 	}
 	if (flags.histogram) {
 		histogram = malloc(sizeof(unsigned) * MAX_CLASSES);
@@ -107,15 +101,25 @@ char *argv[];
 	}
 	if (flags.write_view && stat("views_output", &st))
 		if (mkdir("views_output", 0777)) {
-			fprintf(stderr, "Failed to create directory views_output: error code: %d\n", errno);
-			return 9;
+			perror("[main] mkdir");
+			fprintf(stderr, "Failed to create directory: views_output\n");
+			return 4;
 		}
 	load_weights(0);
-	for (count=i=0; i < end; i++)
+	count = 0;
+	for (i=0; i < end; i++)
 		rec_read_file(files[i], cwd, 0);
+	free(files);
 	if (flags.histogram)
 		for (i=0; i < MAX_CLASSES; i++)
-			printf("%c %hd\n", MAP_CLASS(i), histogram[i]);
+			printf("%c %u\n", MAP_CLASS(i), histogram[i]);
+	return 0;
+}
+
+without_points_dirs(f)
+struct dirent *f;
+{
+	return strcmp(f->d_name, ".") && strcmp(f->d_name, "..");
 }
 
 #define ALIGN  \
@@ -129,38 +133,37 @@ char name[], prev_wd[];
 	struct dirent **files_in_dir;
 	struct stat filestat;
 	int i, n;
-
-	if (!flags.read_dirs) {
+	
+	if (stat(name, &filestat)) {
+		perror("[rec_read_file] stat");
+		fprintf(stderr, "Failed to stat %s\n", name);
+		return;
+	}
+	if (!flags.read_dirs || S_ISREG(filestat.st_mode)) {
 		read_img(name, level);
 		return;
 	}
-	if (!stat(name, &filestat)) {
-		if (S_ISDIR(filestat.st_mode)) {
-			if ((n = scandir(name, &files_in_dir, without_points_dirs, alphasort)) < 0)
-				perror("scandir");
-			else {
-				if (flags.view) {
-					ALIGN
-					printf("%s:\n", name);
-				}
-				chdir(name);
-				for (i=0; i < n; i++) {
-					if (flags.rec_dirs)
-						rec_read_file(files_in_dir[i]->d_name, "..", level+1);
-					else if (!stat(files_in_dir[i]->d_name, &filestat)) {
-						if (S_ISREG(filestat.st_mode))
-							read_img(files_in_dir[i]->d_name, level+1);
-					} else
-						fprintf(stderr, "Failed to stat %s\n", files_in_dir[i]->d_name);
-					free(files_in_dir[i]);
-				}
-				free(files_in_dir);
-				chdir(prev_wd);
-			}
-		} else 
-			read_img(name, level);
-	} else
-		fprintf(stderr, "Failed to stat %s\n", name);
+	if (!flags.rec_dirs && level)
+		return;
+	if ((n = scandir(name, &files_in_dir, without_points_dirs, alphasort)) < 0) {
+		perror("[rec_read_file] scandir");
+		return;
+	}
+	if (flags.view) {
+		ALIGN
+		printf("%s:\n", name);
+	}
+	if (chdir(name)) {
+		perror("[rec_read_file] chdir");
+		return;
+	}
+	for (i=0; i < n; i++) {
+		rec_read_file(files_in_dir[i]->d_name, "..", level+1);
+		free(files_in_dir[i]);
+	}
+	free(files_in_dir);
+	if (chdir(prev_wd))
+		perror("[rec_read_file] chdir");
 }
 
 struct map_prob {
@@ -179,13 +182,13 @@ char name[];
 {
 	void write_png();
 	static float img[INPUT_QTT];
+	char view_name[PATH_MAX + NAME_MAX];
 	float pred;
 	int error, i, j, cn;
 	unsigned char class;
-	char view_name[PATH_MAX+NAME_MAX];
 
 	if ((error = read_png_file(name, &img, flags.verbose))) {
-		fprintf(stderr, "Error: %d. Cannot read file: %s\n", error, name);
+		fprintf(stderr, "[read_img] Error: %d. Cannot read file: %s\n", error, name);
 		return;
 	}
 	if (flags.write_view) {
@@ -194,9 +197,9 @@ char name[];
 		for (j=i=0; name[i]; i++)
 			if (name[i] == '/')
 				j=i+1;
-		cn += snprintf(view_name+cn, NAME_MAX-cn, "%s", name+j);
-		if (cn > NAME_MAX)
-			sprintf(view_name+NAME_MAX-5, ".png");
+		cn += snprintf(view_name+cn, PATH_MAX + NAME_MAX - cn, "%s", name+j);
+		if (cn > PATH_MAX + NAME_MAX)
+			sprintf(view_name + PATH_MAX + NAME_MAX - 5, ".png");
 		write_png(img, view_name);
 	}
 	run(img);
@@ -230,7 +233,10 @@ char name[];
         png_bytepp rows;
         int i, j;
 	
-        fp = fopen(name, "wb");
+	if (!(fp = fopen(name, "rb"))) {
+		fprintf(stderr, "[write_png] File %s could not be opened for reading\n", name);
+		return;
+	}
         png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
         info = png_create_info_struct(png);
         png_init_io(png, fp);
